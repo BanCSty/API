3040643cf812338776790d045c4e25c70c953586
Rax <Rax@DESKTOP-41U8OV7>
2024-05-04 01:51:15 +0300

    [API.Application] Add delete legal entity command

---
+++ API.Application/LegalEntitys/Command/DeleteLegalEntity/DeleteLegalEntityCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using API.Application.Common.Exceptions;
+using API.Application.Interfaces;
+using API.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.LegalEntitys.Command.DeleteLegalEntity
+{
+    public class DeleteLegalEntityCommandHandler
+        : IRequestHandler<DeleteLegalEntityCommand>
+    {
+        private readonly IApiDbContext _dbContext;
+
+        public DeleteLegalEntityCommandHandler(IApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteLegalEntityCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Founders
+                .FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null || entity.Id != request.Id)
+            {
+                throw new NotFoundException(nameof(LegalEntity), request.Id);
+            }
+
+            _dbContext.Founders.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            //Пустой ответ(для тестирования)
+            return Unit.Value;
+        }
+    }
+}
---
+++ API.Application/LegalEntitys/Command/DeleteLegalEntity/DeleteLegalEntityCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.LegalEntitys.Command.DeleteLegalEntity
+{
+    public class DeleteLegalEntityCommandValidator : AbstractValidator<DeleteLegalEntityCommand>
+    {
+        public DeleteLegalEntityCommandValidator()
+        {
+            RuleFor(deleteLECommand => deleteLECommand.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
