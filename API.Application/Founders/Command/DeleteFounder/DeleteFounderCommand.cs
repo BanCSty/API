50031981c7445cd6d34ebb9fa150d4fc0f11829d
Rax <Rax@DESKTOP-41U8OV7>
2024-05-04 00:31:58 +0300

    [API.Application] Add delete founder cvommand

---
+++ API.Application/Founders/Command/DeleteFounder/DeleteFounderCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.DeleteFounder
+{
+    public class DeleteFounderCommand : IRequest
+    {
+        public Guid FounderId { get; set; }
+    }
+}
---
+++ API.Application/Founders/Command/DeleteFounder/DeleteFounderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using API.Application.Common.Exceptions;
+using API.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.DeleteFounder
+{
+    public class DeleteFounderCommandHandler 
+        : IRequestHandler<DeleteFounderCommand>
+    {
+        private readonly IApiDbContext _dbContext;
+
+        public DeleteFounderCommandHandler(IApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteFounderCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Founders
+                .FindAsync(new object[] { request.FounderId }, cancellationToken);
+
+            if (entity == null || entity.Id != request.FounderId)
+            {
+                throw new NotFoundException(nameof(Founders), request.FounderId);
+            }
+
+            _dbContext.Founders.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            //Пустой ответ(для тестирования)
+            return Unit.Value;
+        }
+    }
+}
