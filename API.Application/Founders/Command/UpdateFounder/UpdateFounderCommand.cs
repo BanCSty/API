fb89038dc266627d6ac8c8c97faa2570a2a90018
Rax <Rax@DESKTOP-41U8OV7>
2024-05-04 00:32:22 +0300

    [API.Application] Add update founder command

---
+++ API.Application/Founders/Command/UpdateFounder/UpdateFounderCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.UpdateFounder
+{
+    public class UpdateFounderCommand : IRequest
+    {
+        public Guid FounderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
---
+++ API.Application/Founders/Command/UpdateFounder/UpdateFounderCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using API.Application.Common.Exceptions;
+using API.Application.Interfaces;
+using API.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.UpdateFounder
+{
+    public class UpdateFounderCommandHandler
+        : IRequestHandler<UpdateFounderCommand>
+    {
+        private readonly IApiDbContext _dbContext;
+
+        public UpdateFounderCommandHandler(IApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(UpdateFounderCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = 
+                await _dbContext.Founders.FirstOrDefaultAsync
+                (founder => founder.Id == request.FounderId, cancellationToken);
+
+            if (entity == null || entity.Id != request.FounderId)
+            {
+                throw new NotFoundException(nameof(Founder), request.FounderId);
+            }
+
+            entity.FirstName = request.FirstName;
+            entity.LastName = request.LastName;
+            entity.MiddleName = request.MiddleName;
+            entity.DateUpdate = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
