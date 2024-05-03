17e6c5c8fab98cc954051f3b6cc58c11cd9a66e4
Rax <Rax@DESKTOP-41U8OV7>
2024-05-03 19:27:44 +0300

    [API.Application] Add interface IApiDbContext

---
+++ API.Application/Interfaces/IApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Interfaces
+{
+    public interface IApiDbContext
+    {
+        DbSet<Founder> Founders { get; set; }
+        DbSet<LegalEntity> LegalEntitys { get; set; }
+        DbSet<IndividualEntrepreneur> IndividualEntrepreneurs { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
