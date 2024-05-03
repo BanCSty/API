9ce1572fd1531d00932adbef6047ce609752ed58
Rax <Rax@DESKTOP-41U8OV7>
2024-05-04 00:59:13 +0300

    [API.Application] Add GetFounderDetails query

---
+++ API.Application/Founders/Queries/GetFoundDetails/GetFounderDetailsQuery.cs
@@ -0,0 +1,11 @@
+﻿using API.Domain;
+using MediatR;
+using System;
+
+namespace API.Application.Founders.Queries.GetFoundDetails
+{
+    public class GetFounderDetailsQuery : IRequest<Founder>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ API.Application/Founders/Queries/GetFoundDetails/GetFounderDetailsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using API.Application.Common.Exceptions;
+using API.Application.Interfaces;
+using API.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Queries.GetFoundDetails
+{
+    public class GetFounderDetailsQueryHandler
+        : IRequestHandler<GetFounderDetailsQuery, Founder>
+    {
+        private readonly IApiDbContext _dbContext;
+
+        public GetFounderDetailsQueryHandler(IApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Founder> Handle(GetFounderDetailsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Founders
+                .FirstOrDefaultAsync(founder =>
+                founder.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.Id != request.Id)
+            {
+                throw new NotFoundException(nameof(Founder), request.Id);
+            }
+
+            return entity;
+        }
+    }
+}
