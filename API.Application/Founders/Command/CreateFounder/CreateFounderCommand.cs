7728fe8d0f9d1581b8b5b84a30a26ba240ae780a
Rax <Rax@DESKTOP-41U8OV7>
2024-05-04 00:11:00 +0300

    [API.Application] Add createFounder command

---
+++ API.Application/Founders/Command/CreateFounder/CreateFounderCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.CreateFounder
+{
+    //Содержит информацию, о том, что необходимо для создания учредителя
+    public class CreateFounderCommand : IRequest<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
---
+++ API.Application/Founders/Command/CreateFounder/CreateFounderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using API.Application.Interfaces;
+using API.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Application.Founders.Command.CreateFounder
+{
+    public class CreateFounderCommandHandler
+        : IRequestHandler<CreateFounderCommand, Guid>
+    {
+        private readonly IApiDbContext _dbContext;
+
+        public CreateFounderCommandHandler(IApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(CreateFounderCommand request, CancellationToken cancellationToken)
+        {
+            var founder = new Founder
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                MiddleName = request.MiddleName,
+                DateCreate = DateTime.Now,
+                DateUpdate = null
+            };
+
+            await _dbContext.Founders.AddAsync(founder);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return founder.Id;
+        }
+    }
+}
