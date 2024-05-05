c1e8fcf0aa41370f02e82832feac60f340e8bcbd
Rax <Rax@DESKTOP-41U8OV7>
2024-05-06 02:28:14 +0300

    [API.Application] Change DI

--- API.Application/DependencyInjection.cs
+++ API.Application/DependencyInjection.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using API.Application.Common.Behaviors;
+using API.Application.Founders.Command.CreateFounder;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
 
 namespace API.Application
 {
@@ -7,6 +13,12 @@ namespace API.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services
+                .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
             return services;
         }
     }
