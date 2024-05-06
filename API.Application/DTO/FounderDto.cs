3517dab11c15656a03ee585eff14ae767643c697
Rax <Rax@DESKTOP-41U8OV7>
2024-05-06 14:09:24 +0300

    [API.Application] Add DTO base entity classes for responses

---
+++ API.Application/DTO/FounderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.DTO
+{
+    public class FounderDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public long INN { get; set; }
+    }
+}
---
+++ API.Application/DTO/LegalEntityDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.DTO
+{
+    public class LegalEntityDto
+    {
+        public Guid Id { get; set; }
+        public long INN { get; set; }
+        public string Name { get; set; }
+    }
+}
