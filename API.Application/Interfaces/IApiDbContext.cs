3c8ec16616077334d287a234695916f8fbdef388
Rax <Rax@DESKTOP-41U8OV7>
2024-05-13 18:11:35 +0300

    [API.Application] Add Entry method for Explicit Loading

--- API.Application/Interfaces/IApiDbContext.cs
+++ API.Application/Interfaces/IApiDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using API.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,5 +12,7 @@ namespace API.Application.Interfaces
         DbSet<LegalEntity> LegalEntitys { get; set; }
         DbSet<IndividualEntrepreneur> IndividualEntrepreneurs { get; set; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
     }
 }
