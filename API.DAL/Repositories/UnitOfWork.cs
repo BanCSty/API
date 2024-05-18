411190303d152fd4a41d0281301d9c67dd272b8c
Rax <Rax@DESKTOP-41U8OV7>
2024-05-18 15:33:09 +0300

    [API.DAL] Implement unit of work Interface, for manage state data

---
+++ API.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,77 @@
+﻿using API.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApiDbContext _dbContext;
+        private IDbContextTransaction _currentTransaction;
+
+        public UnitOfWork(ApiDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            if (_currentTransaction != null)
+            {
+                return _currentTransaction;
+            }
+
+            _currentTransaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+            return _currentTransaction;
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                _currentTransaction?.Commit();
+            }
+            catch
+            {
+                await RollbackTransactionAsync();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
