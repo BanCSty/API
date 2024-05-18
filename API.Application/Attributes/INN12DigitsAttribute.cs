a301b8f8ac6dfba8068808cec4fa02376cc8c1b7
Rax <Rax@DESKTOP-41U8OV7>
2024-05-18 15:27:48 +0300

    [API.Application] Delete automapper. Now for mapping i using constructor.

---
+++ API.Application/Attributes/INN12DigitsAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace API.WebApi.Attributes
+{
+    public class INN12DigitsAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            string inn = value.ToString();
+            if (!inn.All(char.IsDigit))
+            {
+                return new ValidationResult("INN must be a number");
+            }
+
+            if (inn.Length != 12)
+            {
+                return new ValidationResult("INN must be a 12 digit number");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
